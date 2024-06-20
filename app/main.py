c504920f4ab0b1fec7f3ff09e2174b73cf15ee39
Hunter Droegkamp <hdroegkamp@gmail.com>
2024-06-20 13:35:37 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
-
+import concurrent.futures
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,13 +11,18 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
 
-    while True:
-        data = conn.recv(1024)
-        if data:
-            conn.send(b"+PONG\r\n")
-        else:
-            break
+    pong = "+PONG\r\n"
+
+    def send_pong():
+        while True:
+            data = conn.recv(1024)
+            if data:
+                conn.send(pong)
+            else:
+                break
     
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(send_pong)
 
 
 if __name__ == "__main__":
